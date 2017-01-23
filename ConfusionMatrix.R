2f28dc2dc60eefe9c8c94269ab9fcf972d2ab711
beyhangl <beyhangl@gmail.com>
2017-01-23 11:56:13 +0300

    confusion matrix

--- ConfusionMatrix.R
+++ ConfusionMatrix.R
@@ -10,6 +10,7 @@ test <- shuffled[test_indices, ]
 
 library(rpart)
 
+
 train<-as.data.frame(train)
 test<-as.data.frame(test)
 tree <- rpart(Survived ~ ., data = mydata, method = "class")
@@ -18,7 +19,6 @@ pred <- predict(tree, test, type = "class")
 
 conf<-table(test$Survived, pred)
 conf
-pred
 #   0   1
 #0 150  13
 #1  11  93
